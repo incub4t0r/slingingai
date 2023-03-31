bcca49ca03db42b1111aba65e0e1d9a185d3ee67
incub4t0r <dchung0812@gmail.com>
2023-03-30 21:58:36 -0400

    updated textarea and css

--- src/javascript/textarea_sizer.js
+++ src/javascript/textarea_sizer.js
@@ -2,3 +2,25 @@
 //     // this.style.height = 'auto';
 //     this.style.height = (this.scrollHeight)+ 6 + 'px';
 // });
+
+// on window load set height of textarea to scrollHeight
+// $(window).on('load', 'textarea[data-at-expandable]', function() {
+//     this.style.height = (this.scrollHeight+2) + 'px';
+// });
+
+$('body').on('keydown input', 'textarea[data-at-expandable]', function() {
+    //Auto-expanding textarea
+    this.style.removeProperty('height');
+    this.style.height = (this.scrollHeight+3) + 'px';
+})
+.on('mousedown', 'textarea[data-at-expandable]', function() {
+    //Do this on focus, to allow textarea to animate to height...
+    this.style.removeProperty('height');
+    this.style.height = (this.scrollHeight+3) + 'px';
+});
+
+// remove height on focusout and set to default height
+$('body').on('focusout', 'textarea[data-at-expandable]', function() {
+    this.style.removeProperty('height');
+    this.style.height = 'auto';
+});
